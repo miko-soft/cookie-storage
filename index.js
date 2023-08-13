fdc41b7b7ce739f1cb6f067bc690b7d9bcef0488
Sasa M <smikodanic@gmail.com>
2023-08-13 15:28:47 +0200

    fix

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ export { BrowserCookie, BrowserStorage };
 
 
 // window
-console.log(typeof window);
 if (typeof window !== 'undefined') {
   if (!window.mikosoft) { window.mikosoft = {}; }
   window.mikosoft.browserStorage = new BrowserStorage();
